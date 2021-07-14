805cf37ab9aa8f935577d5190482031d9942d161
Nick <nick.jablonka@paddle.com>
2021-07-14 22:39:02 +0100

    Pricing page

---
+++ components/PricingCard.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+
+const PricingCard = (props) => {
+	return (
+		<Container>
+			<HeaderWrapper>
+				<Tag>{props.tag}</Tag>
+				<Price>{props.price}</Price>
+				<VAT>{props.vat}</VAT>
+			</HeaderWrapper>
+			<Button>Get this package</Button>
+		</Container>
+	);
+};
+
+export default PricingCard;
+
+const Container = styled.div`
+	width: 28%;
+	background-color: #111827;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding-top: 3rem;
+	padding-bottom: 3rem;
+	border-radius: 15px;
+	border: ${(props) =>
+		props.primary ? "1px solid #22d3ee;" : "1px solid #38404a"};
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	margin-left: 1rem;
+`;
+
+const HeaderWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const Tag = styled.div`
+	font-size: 14px;
+	text-transform: uppercase;
+	color: white;
+`;
+
+const Price = styled.div`
+	font-size: 48px;
+	color: ${(props) => (props.primary ? "#22d3ee;" : "white")};
+	font-weight: 800;
+`;
+
+const VAT = styled.div`
+	font-size: 14px;
+	color: #9ca3af;
+`;
+
+const Button = styled.button`
+	width: 80%;
+	outline: none;
+	border-radius: 8px;
+	background-color: #22d3ee;
+	color: #111827;
+	border: none;
+	padding: 13px 9px;
+	font-size: 14px;
+	font-weight: 500;
+	margin-top: 3rem;
+	cursor: pointer;
+	&:hover {
+		background-color: #52e4fa;
+	}
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,68 @@
+import styled from "styled-components";
+import PricingCard from "../components/PricingCard";
+
 export default function Home() {
 	return (
-		<div>
-			<div>Hello</div>
-		</div>
+		<Container>
+			<HeroWrapper>
+				<Heading>Buy once, use it forever.</Heading>
+				<Subheading>
+					Get lifetime access to 400+ components today, plus any updates we
+					release in the future for a simple one-time price.
+				</Subheading>
+			</HeroWrapper>
+			<PricingContainer>
+				<PricingCard
+					tag={"APPLICATION UI"}
+					price={"£119"}
+					vat={"(+£23.80 VAT)"}></PricingCard>
+				<PricingCard
+					primary
+					tag={"APPLICATION UI + MARKETING"}
+					price={"189"}
+					vat={"(+£37.80 VAT)"}></PricingCard>
+				<PricingCard
+					tag={"MARKETING"}
+					price={"£119"}
+					vat={"(+£23.80 VAT)"}></PricingCard>
+			</PricingContainer>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	background-image: url("/beams-bottom.jpg");
+	background-size: cover;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+const HeroWrapper = styled.div`
+	width: 50%;
+`;
+
+const Heading = styled.div`
+	font-weight: 800;
+	font-size: 54px;
+	color: white;
+	text-align: center;
+`;
+
+const Subheading = styled.div`
+	margin-top: 1rem;
+	font-size: 18px;
+	color: #d1d5db;
+	text-align: center;
+`;
+
+const PricingContainer = styled.div`
+	width: 90%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 3rem;
+`;
